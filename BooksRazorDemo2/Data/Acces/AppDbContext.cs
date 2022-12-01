994667822d3f90116197d5258ab32f585cda7ed4
Selkan Larlar Trainer <sl@trainer.com>
2022-12-01 09:35:15 +0300

    AppDb Context oluşturuldu

---
+++ BooksRazorDemo2/Data/Acces/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BooksRazorDemo2.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksRazorDemo2.Data.Acces
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>  options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
