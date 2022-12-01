10be2fa98a111bed3c5f6c079c141d2d9b2c4b41
Selkan Larlar Trainer <sl@trainer.com>
2022-12-01 09:22:16 +0300

    Add project files.

---
+++ BooksDemoRazor/Data/Acces/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BooksDemoRazor.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksDemoRazor.Data.Acces
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext>  options) :base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BooksDemoRazor/Data/Model/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace BooksDemoRazor.Data.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
