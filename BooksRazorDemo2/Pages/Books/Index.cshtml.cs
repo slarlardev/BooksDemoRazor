d98eb36c981555798f5b585de00b1bfcf68948d4
Selkan Larlar Trainer <sl@trainer.com>
2022-12-01 09:24:01 +0300

    Books klsör index eklendi

---
+++ BooksRazorDemo2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BooksRazorDemo2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
