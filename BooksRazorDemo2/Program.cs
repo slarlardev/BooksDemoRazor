55de8a55742db164951d0219420f0f6086c794ad
Selkan Larlar Trainer <sl@trainer.com>
2022-12-01 09:50:27 +0300

    Database DI (dependency injection)

--- BooksRazorDemo2/Program.cs
+++ BooksRazorDemo2/Program.cs
@@ -1,7 +1,16 @@
+using BooksRazorDemo2.Data.Acces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDbContext>(
+    options => options
+    .UseSqlServer(
+        builder.Configuration.GetConnectionString("MyConn")
+        )
+    );
 
 var app = builder.Build();
 
