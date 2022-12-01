b2e9eaa65594cc169e653e5b250d59bec3fde476
Selkan Larlar Trainer <sl@trainer.com>
2022-12-01 10:41:15 +0300

    Books index çalışıyor

--- BooksRazorDemo2/Pages/Books/Index.cshtml.cs
+++ BooksRazorDemo2/Pages/Books/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BooksRazorDemo2.Data.Acces;
+using BooksRazorDemo2.Data.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace BooksRazorDemo2.Pages.Books
 {
     public class IndexModel : PageModel
     {
+        public AppDbContext Db { get; set; }
+        public IEnumerable<Book>? Books { get; set; }
+
+        public IndexModel(AppDbContext db)
+        {
+            Db = db;
+        }
         public void OnGet()
         {
+            Books = Db.Books;
         }
     }
 }
